59d3a6804dc4a6ea8ea21a2a9c4161e672984545
Li_Wan_Bin <164940880@qq.com>
2020-06-10 16:34:51 +0800

    init

---
+++ pages/home/home.js
@@ -0,0 +1,76 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    ad: []
+  },
+  changeAd(ad) {
+    this.setData({
+      data: [{
+        img: '',
+        type: 'movie',
+        title: '战狼',
+        des: '人生啦啦啦',
+        createTime: '2020-06-10 14:11:12',
+        like: 124,
+        author: '吴京'
+      }]
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(options)
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
